68993c06cc3b99c5c7219298a11949ef9bc30884
2ll2sij <2ll2sij@gmail.com>
2017-01-23 00:57:08 +0800

    Create Secondpage.java

---
+++ android_apps/List/Secondpage.java
@@ -0,0 +1,15 @@
+package com.example.wonsoyoung.myapplication05_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Secondpage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_pg);
+
+    }
+
+}
